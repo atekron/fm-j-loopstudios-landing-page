22db89154187409e00e91d900f55ad57e4accda8
Michael Kret <dirtysculptor@gmail.com>
2021-05-30 15:44:52 +0300

    mobile menu done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("hello");
+
+function mobileMenu() {
+  console.log("hi");
+  const hamburgerBtn = document.querySelector(".header__mobile-hamburger");
+  const closeBtn = document.querySelector(".header__mobile-close");
+  const mobileMenu = document.querySelector(".header__links");
+
+  if (hamburgerBtn.style.display !== "none") {
+    hamburgerBtn.style.display = "none";
+    closeBtn.style.display = "inline-block";
+    mobileMenu.style.display = "flex";
+  } else {
+    hamburgerBtn.style.display = "inline-block";
+    closeBtn.style.display = "none";
+    mobileMenu.style.display = "none";
+  }
+}
